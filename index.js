1f4a237ea09b79a902b7e5d67f116beca3dc0858
Bimal Patel <bimp9070@gmail.com>
2021-10-07 12:54:28 -0600

    more problems

--- index.js
+++ index.js
@@ -40,3 +40,16 @@ console.log(array3);
           else
             return false;
         }
+
+//Problem 3: Write a function that accepts an array of 10 integers (between 0 and 9),
+          //that returns a string of those numbers in the form of a phone number.
+
+          function createPhoneNumber(numbers){
+            numbers.unshift('('); 
+            numbers.splice(4,0,')');
+            numbers.splice(5,0," ");
+            numbers.splice(9,0,'-');
+            
+            return numbers.join("");
+          }
+

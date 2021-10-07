1e8032a0410dc6eca0fe8fb4d85c2726326f815e
Bimal Patel <bimp9070@gmail.com>
2021-10-07 12:47:21 -0600

    "Problem 2"

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ let array3=array1.filter(word=>{
 
 console.log("array1"+array1);
 console.log(array3);
+
+//Problem 2:Write a function that takes a string of parentheses, and determines 
+        // if the order of the parentheses is valid. The function should return true 
+        // if the string is valid, and false if it's invalid.
+
+        function validParentheses(parens) {
+          // your code here ..
+          let arr=[];
+          for(let i=0;i<parens.length;i++){
+            if(parens.charAt(i)=='(')
+              arr.push(1);
+            else{
+              if(arr.length==0)
+                return false;
+              else
+                arr.pop();
+            }
+          }
+          if(arr.length==0)
+            return true;
+          else
+            return false;
+        }
